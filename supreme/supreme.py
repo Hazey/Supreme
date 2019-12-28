90b98be591fc4fc162bea36e4e866dbeddfc678a
Hazey <35635541+Hazey@users.noreply.github.com>
2019-12-28 16:24:44 +0000

    Update supreme.py

--- supreme/supreme.py
+++ supreme/supreme.py
@@ -9,4 +9,4 @@ from .models import ItemInfo
 class Supreme(API):
     async def items(self) -> List[ItemInfo]:
         json = await self.json_request("GET", "mobile_stock.json")
-        return [ItemInfo(**item) for item in chain.from_iterable(json["products_and_categories"].values())]
+        return [ItemInfo(**i) for i in chain.from_iterable(json["products_and_categories"].values())]
